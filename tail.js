386f7143cd6fdf94f517d3a272552add2fb7a17f
Cynthia Lam <cynthia-lam@outlook.com>
2024-01-27 02:02:06 -0500

    Added tail.js

---
+++ tail.js
@@ -0,0 +1,22 @@
+const _ = require('lodash');
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return _.tail(array);
+};
+
+const tailTest1 = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(tailTest1.length, 2);
+assertEqual(tailTest1[0], 'Lighthouse');
+assertEqual(tailTest1[1], 'Labs');
+
+const tailTest2 = tail([]);
+assertEqual(tailTest2.length, 0);
