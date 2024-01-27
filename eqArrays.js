0bd52272368a2ee00988601fe6e0e35d2e8a7b22
Cynthia Lam <cynthia-lam@outlook.com>
2024-01-27 16:46:17 -0500

    Added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+    
+  for (let i = 0; i <= array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+        return false;
+    }
+  }
+  
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));  // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+console.log(eqArrays([1, 3], [1, 2, 3])); // => false
+console.log(eqArrays([], [])); // => true
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
