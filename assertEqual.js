6a5936b13de1acefe724078f2610eb97082fdb67
Cynthia Lam <cynthia-lam@outlook.com>
2024-01-27 01:34:57 -0500

    Changed else if to else

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
     console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else if (actual === expected) {
+  } else {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
