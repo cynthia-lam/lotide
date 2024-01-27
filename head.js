7143030080e5dc938b4b8fd158da188e8916e7ac
Cynthia Lam <cynthia-lam@outlook.com>
2024-01-27 01:52:13 -0500

    Added head.js

---
+++ head.js
@@ -0,0 +1,25 @@
+const _ = require('lodash');
+
+// assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// assertEqual tests
+
+// assertEqual(1, 1);
+// assertEqual(1, '1');
+// assertEqual(1, 2);
+// assertEqual('2', '2');
+
+// head function - returns the first item of an array
+const head = function(array) {
+  return _.head(array);
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
