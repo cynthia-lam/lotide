9861a63228facd3922cebac8869dc528c13979d4
Cynthia Lam <cynthia-lam@outlook.com>
2024-01-27 01:28:25 -0500

    Added more text and tests

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,15 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log('🛑 Assertion Failed');
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   } else if (actual === expected) {
-    console.log('✅ Assertion Passed');
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
 // TEST CODE
 
 assertEqual(1, 1);
+assertEqual(1, '1');
+assertEqual(1, 2);
+assertEqual('2', '2');
