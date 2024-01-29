ee161c81589314e4978cc2ed2032ef23a21824ea
Cynthia <cynthia-lam@outlook.com>
2024-01-28 19:03:18 -0500

    Added assertArraysEqual.ls

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,27 @@
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+    return false;
   }
     
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
-    } else {
-      
+        return false;
     }
   }
-  // need to move this so it doesnt print every time
-  console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  
+  return true;
 };
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+
+
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
@@ -22,3 +31,5 @@ assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertArraysEqual([1, 3], [1, 2, 3]); // => false
 assertArraysEqual([], []); // => true
+
+// take in two arrays and console log if 
