cc8b97a7d2e0f6fec03847b09801196e43511ead
Cynthia <cynthia-lam@outlook.com>
2024-02-14 07:12:13 -0500

    Removed unused require, added more tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,15 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const tailTest1 = tail(['Hello', 'Lighthouse', 'Labs']);
+const array1 = ['Hello', 'Lighthouse', 'Labs']
+const array2 = array1;
+const tailTest1 = tail(array1);
 assertEqual(tailTest1.length, 2);
 assertEqual(tailTest1[0], 'Lighthouse');
 assertEqual(tailTest1[1], 'Labs');
+console.log('Checking if array is unchanged');
+assertEqual(array1, array2);
 
+console.log("Testing empty array");
 const tailTest2 = tail([]);
 assertEqual(tailTest2.length, 0);
