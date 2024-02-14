58775271f5c60a14048adce76767b04be5427a24
Cynthia <cynthia-lam@outlook.com>
2024-02-14 07:15:09 -0500

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -21,13 +15,6 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));  // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-console.log(eqArrays([1, 3], [1, 2, 3])); // => false
-console.log(eqArrays([], [])); // => true
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
