6ba87c3fb095c7e8cac1af21375494c718b3e7a1
Cynthia <cynthia-lam@outlook.com>
2024-02-14 20:15:48 -0500

    Refactored headTest, middleTest with M&C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,24 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () =>{
+  it("should return empty array for [1]", () =>{
+    assert.deepEqual(middle([1]), []);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it("should return empty array for [1, 2]", () =>{
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  it("should return [2]] for [1, 2, 3]", () =>{
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("should return [2, 3] for [1, 2, 3, 4]", () =>{
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+
+});
+
+
